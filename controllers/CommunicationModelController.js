38c973b13bdbcfd1b6075d476f135d7e9e6ac5e4
nayebare <mnayebare@rwanda.cmu.edu>
2018-07-17 19:06:53 +0300

    saving database

--- controllers/CommunicationModelController.js
+++ controllers/CommunicationModelController.js
@@ -1,9 +1,10 @@
 
+"use strict"
 var CommunicationModel = require('../models/CommunicationModel.js');
 
-module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
-    var communication = new CommunicationModel(email, phone, communicationMode, disaster, level, place, language);
-    CommunicationModel.registerCommunicationModel();
+//module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
+    function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
+          CommunicationModel.registerCommunicationModel();
 }
 
 registerCommunicationModel('email','873','email','fire',3,'Butare','english');

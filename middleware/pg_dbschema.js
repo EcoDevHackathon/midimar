d55271704b0547055544edd07ec78e62973fdb92
nayebare <mnayebare@rwanda.cmu.edu>
2018-07-17 15:06:46 +0300

    database schema

--- middleware/pg_dbschema.js
+++ middleware/pg_dbschema.js
@@ -1,15 +1,10 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://jvnunenmsixtda:fd050031169d5d2adc01d0c0f191a73c8b67a7a9eafbeca96f6973a052e45fcb@ec2-50-19-232-205.compute-1.amazonaws.com:5432/d3o1u74nfnh9jm?ssl=true'
+const connectionString = process.env.DATABASE_URL || 'postgres://jvnunenmsixtda:fd050031169d5d2adc01d0c0f191a73c8b67a7a9eafbeca96f6973a052e45fcb@ec2-50-19-232-205.compute-1.amazonaws.com:5432/d3o1u74nfnh9jm?ssl=true';
+const client = new pg.Client(connectionString);
+client.connect()
 
-pg.connect(connectionString, (err, client, done) => {
-  // Handle connection errors
-  if (err) {
-    console.log(err);
-    // return res.status(500).json({success: false, data: err});
-  }
-  else {
-    console.log('connection successfuly')
-  }
-});
-
-
+/* Test query
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
+*/

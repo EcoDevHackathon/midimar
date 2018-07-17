244f0c959b2f3643aaffdcb8dd1385c299629407
JeannetteMukamanaBayi <jmukaman@andrew.cmu.edu>
2018-07-17 18:05:41 +0200

    database

--- controllers/CommunicationModelController.js
+++ controllers/CommunicationModelController.js
@@ -1,10 +1,12 @@
 
 var CommunicationModel = require('../models/CommunicationModel.js');
 
-module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
-    var communication = new CommunicationModel(email, phone, communicationMode, disaster, level, place, language);
-    CommunicationModel.registerCommunicationModel();
+//module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
+async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
+     var communication = new CommunicationModel(email, phone, communicationMode, disaster, level, place, language);
+    console.log("logs in controllers!!!!!!!!!!!!!!!!");
+    communication.registerCommunicationModel();
 }
 
-registerCommunicationModel('email','873','email','fire',3,'Butare','english');
+registerCommunicationModel('ja@gmail', '873', 'email', 'fire', 3, 'Butare', 'english');
 

8cb286832b8779c51828ab3a17711bffeda4739b
JeannetteMukamanaBayi <jmukaman@andrew.cmu.edu>
2018-07-17 15:08:28 +0200

    commit

---
+++ controllers/CommunicationModelController.js
@@ -0,0 +1,6 @@
+
+var CommunicationModel = require('../models/CommunicationModel.js');
+module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster,level,place, languguage,   callback) {
+ 
+    
+}

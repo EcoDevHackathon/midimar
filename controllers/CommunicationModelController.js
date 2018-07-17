8838e0e9a52222957ba678cdeaa931f7e4990ffb
nayebare <mnayebare@rwanda.cmu.edu>
2018-07-17 16:48:03 +0300

    controller

--- controllers/CommunicationModelController.js
+++ controllers/CommunicationModelController.js
@@ -4,5 +4,7 @@ var CommunicationModel = require('../models/CommunicationModel.js');
 module.exports.registerCommunicationModel = async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
     var communication = new CommunicationModel(email, phone, communicationMode, disaster, level, place, language);
     CommunicationModel.registerCommunicationModel();
-
 }
+
+registerCommunicationModel('email','873','email','fire',3,'Butare','english');
+

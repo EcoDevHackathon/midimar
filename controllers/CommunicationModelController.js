0a036f4f49d4414ade1d94bf3038c7c690713034
JeannetteMukamanaBayi <jmukaman@andrew.cmu.edu>
2018-07-18 13:21:46 +0200

    multiple messages

--- controllers/CommunicationModelController.js
+++ controllers/CommunicationModelController.js
@@ -1,44 +1,36 @@
 
 "use strict"
 var CommunicationModel = require('../models/CommunicationModel.js');
-const nodemailer = require('nodemailer');
+var Message = require('../models/Message.js');
 
 async function registerCommunicationModel(email, phone, communicationMode, disaster, level, place, language, callback) {
+    console.log("registering communication mode");
     var communication = new CommunicationModel(email, phone, communicationMode, disaster, level, place, language);
     communication.registerCommunicationModel();
 }
-//registerCommunicationModel('rodger@gmail.com', '873757575', 'email', 'fire', 3, 'Butare', 'english');
+//registerCommunicationModel('rodger@gmail.com', '873757575', 'email', 'fire now', 3, 'Butare', 'english');
 
 
 //send email function
-function sendEmailTechnician() {
+function sendTechnicalEmail(mailOptions) {
     // Generate test SMTP service account from gmail
-    nodemailer.createTestAccount((err, account) => {
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user: 'midimaralert@gmail.com',
-                pass: 'h0zg0Kkbupe$B3!B'
-            }
-        });
-        let mailOptions = {
-            from: 'MIDIMAR DISASTER ALERT <midimaralert@gmail.com>',
-            to: 'micheal.nayebare@gmail.com',
-            subject: 'Midimar Disaster Alert',
-            text: 'Heavy Rainfall shower',
-            html: '<b>Heavy Raain Expected on such a day</b>'
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
-    });
+    Message.sendTechnicalEmail(mailOptions)
 }
 
+/*
+let mailOptions =[{
+    from: 'MIDIMAR DISASTER ALERT <midimaralert@gmail.com>',
+    to: 'jannekista@gmail.com',
+    subject: 'Midimar Disaster Alert',
+    text: 'Heavy Rainfall shower',
+    html: '<b>Heavy Raain Expected on such a day</b>'
+}, {
+    from: 'MIDIMAR DISASTER ALERT <midimaralert@gmail.com>',
+    to: 'jannekista3@gmail.com',
+    subject: 'Midimar Disaster Alert',
+    text: 'Heavy Rainfall shower',
+    html: '<b>Heavy Raain Expected on such a day</b>'
+}] ;
 //call the function
-sendEmailTechnician();
+sendTechnicalEmail(mailOptions);
+*/

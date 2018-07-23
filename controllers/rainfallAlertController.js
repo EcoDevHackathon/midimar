bedfebc493ba86f19680615fb8d5a254ba012c55
nayebare <mnayebare@rwanda.cmu.edu>
2018-07-24 00:09:19 +0300

    completing code

--- controllers/rainfallAlertController.js
+++ controllers/rainfallAlertController.js
@@ -3,38 +3,36 @@
 var rainfallalertModel = require('../models/rainfallalertModel.js');
 var request = require('request');
 
-module.exports.sendRainfallAlert  = async function sendRainfallAlert(alertType, startExpectedTime, endExpectedTime,  description, alertSeverity,alertId, date,rainfalAmount, rainfallIntensity,district, sector, callback) {
+module.exports.sendRainfallAlert = async function sendRainfallAlert(alertType, startExpectedTime, endExpectedTime, description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector, callback) {
     console.log("controller......");
-    var rainfallAlert = new rainfallalertModel(alertType, startExpectedTime, endExpectedTime,  description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector);
-    rainfallAlert.registerRainfallAlert(callback);
+    var rainfallAlert = new rainfallalertModel(alertType, startExpectedTime, endExpectedTime, description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector);    rainfallAlert.registerRainfallAlert(callback);
 
 
 }
 
 //disseminating information
-module.exports.disseminate  = async function disseminate(alertType, startExpectedTime, endExpectedTime,  description, alertSeverity,alertId, date,rainfalAmount, rainfallIntensity,district, sector, comment, receivers, callback) {
+module.exports.disseminate = async function disseminate(alertType, startExpectedTime, endExpectedTime, description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector, comment, receivers, callback) {
     console.log("controller......");
-   // var rainfallAlert = new rainfallalertModel(
-    var rainfallAlert = new rainfallalertModel(alertType, startExpectedTime, endExpectedTime,  description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector);
+    // var rainfallAlert = new rainfallalertModel(
+    var rainfallAlert = new rainfallalertModel(alertType, startExpectedTime, endExpectedTime, description, alertSeverity, alertId, date, rainfalAmount, rainfallIntensity, district, sector);
     rainfallAlert.disseminate(comment, receivers, callback);
 }
-
+//alerts model
 module.exports.getAlerts = function getAlerts(callback) {
-    console.log("controllers .... alerts") 
+    console.log("controllers .... alerts")
     rainfallalertModel.getAlerts(callback);
 }
-
+//get full report  model
 module.exports.getReport = function getReport(region, callback) {
-    console.log("controllers .... report") 
+    console.log("controllers .... report")
     rainfallalertModel.getReport(region, callback);
 }
-
+//get coordinates data
 module.exports.getCoordinates = function getCoordinates(callback) {
-    console.log("controllers .... coordinates") 
+    console.log("controllers .... coordinates")
     rainfallalertModel.getCoordinates(callback);
 }
-
-
+//writting a sync functions for sending messages
 async function sendMessage(msg) {
 
 
